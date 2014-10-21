5f7b03f58c0a2cbc9fb6f7310aaa62d569fb2374
David Honess <david.honess+github@gmail.com>
2014-10-21 12:42:07 +0100

    load from credentials.oracle, not credentials

--- upload_to_oracle.py
+++ upload_to_oracle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-credentials_file = os.path.join(os.path.dirname(__file__), "credentials")
+credentials_file = os.path.join(os.path.dirname(__file__), "credentials.oracle")
 if os.path.isfile(credentials_file):
     import database
     db = database.weather_database()
