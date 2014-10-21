b563ff4d3a30de9fa998ebdd82ec418b710208df
David Honess <david.honess+github@gmail.com>
2014-10-21 10:19:41 +0100

    Update tgs2600.py

--- tgs2600.py
+++ tgs2600.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-from MCP342X import *
+import MCP342X
 
 class TGS2600(object):
     def __init__(self, adc_channel = 1):
         self.adc_channel = adc_channel
         
     def get_value(self):
-        adc = MCP342X.shared
+        adc = MCP342X.MCP342X.shared
         adc_value = adc.read(self.adc_channel)
         return (100.0 / adc.max) * (adc.max - adc_value) #as percentage
