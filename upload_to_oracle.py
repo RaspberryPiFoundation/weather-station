e6030f5cd9c1b18ae594619dfe0a623df4197704
David Honess <david.honess+github@gmail.com>
2014-11-27 12:16:11 +0000

    add code to prevent concurrency

--- upload_to_oracle.py
+++ upload_to_oracle.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
-import os
+import os, fcntl
 credentials_file = os.path.join(os.path.dirname(__file__), "credentials.oracle")
 if os.path.isfile(credentials_file):
-    import database
-    db = database.weather_database()
-    db.upload()
+    lock_file = "/var/lock/oracle.lock"
+    f = open(lock_file, 'w')
+    try:
+        fcntl.lockf(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        print "no other uploads in progress, proceeding..."
+        import database
+        db = database.weather_database()
+        db.upload()
+    except IOError:
+        print "another upload is running exiting now"
+    finally:
+        f.close()
+        os.remove(lock_file)
 else:
     print "credentials file not found"
