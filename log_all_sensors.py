0be48bf7986885fe0a3c40765c6e6d8049b504aa
David Honess <david.honess+github@gmail.com>
2015-02-25 14:51:19 +0000

    changes for prototype schematic

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -4,7 +4,7 @@ import database # requires MySQLdb python 2 library which is not ported to pytho
 
 pressure = bmp085.BMP085()
 temp_probe = ds18b20_therm.DS18B20()
-air_qual = tgs2600.TGS2600(adc_channel = 0)
+air_qual = tgs2600.TGS2600(adc_channel = 1)
 humidity = HTU21D.HTU21D()
 wind_dir = wind_direction.wind_direction(adc_channel = 0, margin = 20)
 interrupts = interrupt_client.interrupt_client(port = 49501)
