a66dd46d68bd7f8718d6cbcec9972a75630cf0b2
David Honess <david.honess+github@gmail.com>
2015-02-25 11:49:04 +0000

    changes for prototype schematic

--- tgs2600.py
+++ tgs2600.py
@@ -2,11 +2,11 @@
 import MCP342X
 
 class TGS2600(object):
-    def __init__(self, adc_channel = 0):
+    def __init__(self, adc_channel = 1):
         self.adc_channel = adc_channel
         
     def get_value(self):
-        adc = MCP342X.MCP342X(address = 0x6a)
+        adc = MCP342X.MCP342X(address = 0x69)
         adc_value = adc.read(self.adc_channel)
         return (100.0 / adc.max) * (adc.max - adc_value) #as percentage
 
