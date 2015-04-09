07cbb230e40095f14ed9546f6ed9b43eff2952b4
David Honess <dave@raspberrypi.org>
2015-04-09 14:48:35 +0000

    add temp coefficient compensation

--- HTU21D.py
+++ HTU21D.py
@@ -1,71 +1,89 @@
 #!/usr/bin/python3
-import struct, array, time, i2c_base
+import struct
+import array
+import time
+import i2c_base
 
-HTU21D_ADDR = 0x40 
-CMD_READ_TEMP_HOLD = b"\xE3" 
-CMD_READ_HUM_HOLD = b"\xE5" 
-CMD_READ_TEMP_NOHOLD = b"\xF3" 
-CMD_READ_HUM_NOHOLD = b"\xF5" 
-CMD_WRITE_USER_REG = b"\xE6" 
-CMD_READ_USER_REG = b"\xE7" 
-CMD_SOFT_RESET= b"\xFE" 
+HTU21D_ADDR = 0x40
+CMD_READ_TEMP_HOLD = b"\xE3"
+CMD_READ_HUM_HOLD = b"\xE5"
+CMD_READ_TEMP_NOHOLD = b"\xF3"
+CMD_READ_HUM_NOHOLD = b"\xF5"
+CMD_WRITE_USER_REG = b"\xE6"
+CMD_READ_USER_REG = b"\xE7"
+CMD_SOFT_RESET = b"\xFE"
+
 
 class HTU21D(object):
     def __init__(self):
-        self.dev = i2c_base.i2c(HTU21D_ADDR, 1) #HTU21D 0x40, bus 1
-        self.dev.write(CMD_SOFT_RESET) #soft reset
+        self.dev = i2c_base.i2c(HTU21D_ADDR, 1)  # HTU21D 0x40, bus 1
+        self.dev.write(CMD_SOFT_RESET)  # Soft reset
         time.sleep(.1)
-    def ctemp(self, sensorTemp):
-        tSensorTemp = sensorTemp / 65536.0
-        return -46.85 + (175.72 * tSensorTemp)
-    def chumid(self, sensorHumid):
-        tSensorHumid = sensorHumid / 65536.0
-        return -6.0 + (125.0 * tSensorHumid)
+
+    def ctemp(self, sensor_temp):
+        t_sensor_temp = sensor_temp / 65536.0
+        return -46.85 + (175.72 * t_sensor_temp)
+
+    def chumid(self, sensor_humid):
+        t_sensor_humid = sensor_humid / 65536.0
+        return -6.0 + (125.0 * t_sensor_humid)
+
+    def temp_coefficient(self, rh_actual, temp_actual, coefficient=-0.15):
+        return rh_actual + (25 - temp_actual) * coefficient
+
     def crc8check(self, value):
-        # Ported from Sparkfun Arduino HTU21D Library: 
+        # Ported from Sparkfun Arduino HTU21D Library:
         # https://github.com/sparkfun/HTU21D_Breakout
-        remainder = ( ( value[0] << 8 ) + value[1] ) << 8
+        remainder = ((value[0] << 8) + value[1]) << 8
         remainder |= value[2]
-        
-        # POLYNOMIAL = 0x0131 = x^8 + x^5 + x^4 + 1 divisor = 
-        # 0x988000 is the 0x0131 polynomial shifted to farthest 
+
+        # POLYNOMIAL = 0x0131 = x^8 + x^5 + x^4 + 1 divisor =
+        # 0x988000 is the 0x0131 polynomial shifted to farthest
         # left of three bytes
         divisor = 0x988000
-        
+
         for i in range(0, 16):
-            if( remainder & 1 << (23 - i) ):
+            if(remainder & 1 << (23 - i)):
                 remainder ^= divisor
             divisor = divisor >> 1
-        
+
         if remainder == 0:
             return True
         else:
             return False
-    
+
     def read_temperature(self):
-        self.dev.write(CMD_READ_TEMP_NOHOLD) #measure temp
+        self.dev.write(CMD_READ_TEMP_NOHOLD)  # Measure temp
         time.sleep(.1)
         data = self.dev.read(3)
         buf = array.array('B', data)
         if self.crc8check(buf):
-            temp = (buf[0] << 8 | buf [1]) & 0xFFFC
+            temp = (buf[0] << 8 | buf[1]) & 0xFFFC
             return self.ctemp(temp)
         else:
             return -255
-            
+
     def read_humidity(self):
-        self.dev.write(CMD_READ_HUM_NOHOLD) #measure humidity
+        temp_actual = self.read_temperature()  # For temperature coefficient compensation
+        self.dev.write(CMD_READ_HUM_NOHOLD)  # Measure humidity
         time.sleep(.1)
         data = self.dev.read(3)
         buf = array.array('B', data)
-        
+
         if self.crc8check(buf):
-            humid = (buf[0] << 8 | buf [1]) & 0xFFFC
-            return self.chumid(humid)
+            humid = (buf[0] << 8 | buf[1]) & 0xFFFC
+            rh_actual = self.chumid(humid)
+
+            rh_final = self.temp_coefficient(rh_actual, temp_actual)
+
+            rh_final = 100.0 if rh_final > 100 else rh_final  # Clamp > 100
+            rh_final = 0.0 if rh_final < 0 else rh_final  # Clamp < 0
+
+            return rh_final
         else:
             return -255
-            
+
 if __name__ == "__main__":
     obj = HTU21D()
     print("Temp: %s C" % obj.read_temperature())
-    print("Humid: %s %% rH" %  obj.read_humidity())
+    print("Humid: %s %% rH" % obj.read_humidity())
