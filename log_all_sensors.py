0645261928dbee492d5319383b200b1ad2fc153e
Texy <texyuk64@yahoo.co.uk>
2016-06-11 10:28:43 +0100

    Update log_all_sensors.py

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -11,7 +11,8 @@ print "BME280_humidity =", bme280_humidity
 print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
 
 temp_probe = ds18b20_therm.DS18B20()
-air_qual = tgs2600.TGS2600(adc_channel = 0)
+#air_qual = tgs2600.TGS2600(adc_channel = 0)
+air_qual = 33 # temporary hack
 humidity = HTU21D.HTU21D()
 wind_dir = wind_direction.wind_direction(adc_channel = 0, config_file="wind_direction.json")
 interrupts = interrupt_client.interrupt_client(port = 49501)
@@ -21,7 +22,8 @@ db = database.weather_database() #Local MySQL db
 wind_average = wind_dir.get_value(10) #ten seconds
 
 print("Inserting...")
-db.insert(humidity.read_temperature(), temp_probe.read_temp(), air_qual.get_value(), pressure.get_pressure(), humidity.read_humidity(), wind_average, interrupts.get_wind(), interrupts.get_wind_gust(), interrupts.get_rain())
+#db.insert(humidity.read_temperature(), temp_probe.read_temp(), air_qual.get_value(), pressure.get_pressure(), humidity.read_humidity(), wind_average, interrupts.get_wind(), interrupts.get_wind_gust(), interrupts.get_rain())
+db.insert(humidity.read_temperature(), temp_probe.read_temp(), air_qual, hectopascals, humidity.read_humidity(), wind_average, interrupts.get_wind(), interrupts.get_wind_gust(), interrupts.get_rain())
 print("done")
 
 interrupts.reset()
