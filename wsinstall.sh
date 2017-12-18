09a2c99e70b5f8dc159a5afc3e195c0957923436
topshed <top_shed@yahoo.com>
2017-12-18 10:20:39 +0000

    Hashed out apache and php install

--- wsinstall.sh
+++ wsinstall.sh
@@ -84,7 +84,7 @@ done
 
 echo 'Installing local database'
 sudo apt-get install -y mariadb-server mariadb-client libmariadbclient-dev
-sudo apt-get install -y apache2 php5 libapache2-mod-php5 php-mysql
+## sudo apt-get install -y apache2 php7.0 libapache2-mod-php* php7.0-mysql
 sudo pip3 install mysqlclient
 
 
